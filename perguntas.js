e1980707061c34bc82fef4bfa62b9e67f96c2063
acostaluana <luana.maciel.rodrigues@escola.pr.gov.br>
2025-09-17 16:25:58 -0300

    17-09

--- perguntas.js
+++ perguntas.js
@@ -1,5 +1,11 @@
 criaCartao(
-'Programaçao', 
+'Programação', 
+'O que é Java Script?',
+'Java Script é uma linguagem de Programaçao'
+)
+
+criaCartao(
+'Programação', 
 'O que é Java Script?',
 'Java Script é uma linguagem de Programaçao'
 )
